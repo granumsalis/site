3bd85c0cb2198b84771b24d99e8ffbf3924cc1a5
Ekaterina Zaikina <katriyna@gmail.com>
2015-08-15 20:19:23 +0300

    override default color

--- webpack.config.js
+++ webpack.config.js
@@ -16,7 +16,16 @@ module.exports = {
       {
         test: /\.html$/,
         loader: 'ng-cache'
-      }
+      },
+      { test: /\.png$/, loader: 'url-loader?limit=10000' },
+      { test: /\.gif$/, loader: 'url-loader?limit=10000' },
+      // the url-loader uses DataUrls.
+      // the file-loader emits files.
+      { test: /\.woff$/, loader: 'url-loader?limit=10000&minetype=application/font-woff' },
+      { test: /\.woff2$/, loader: 'url-loader?limit=10000&minetype=application/font-woff' },
+      { test: /\.ttf$/, loader: 'file-loader' },
+      { test: /\.eot$/, loader: 'file-loader' },
+      { test: /\.svg$/, loader: 'url-loader?limit=10000' }
     ]
   }
 };
