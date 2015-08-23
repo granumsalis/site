294972d8835b6af029c39737d0f015916e7b1db5
Ekaterina Zaikina <katriyna@gmail.com>
2015-08-23 20:29:48 +0300

    do not store stamplay key in code

--- app/thinkslow.js
+++ app/thinkslow.js
@@ -8,7 +8,7 @@ thinkSlowAppModule.constant('Stamplay', window.Stamplay);
 thinkSlowAppModule.config([
     'Stamplay',
     function(Stamplay) {
-      Stamplay.init('7c756fe1e05a6a9742fdba24a116ad5c14f7cbb2da974889fcb962f74c2a0226');
+      Stamplay.init('thinkslow');
     }
 ]);
 
